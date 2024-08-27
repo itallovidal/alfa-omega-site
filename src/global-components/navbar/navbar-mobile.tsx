957d8513c7e2d93f1943b66df64c5afd3da5ba20
italloVidal <itallovidalll@gmail.com>
2024-08-27 02:47:07 -0300

    navbar mobilke

--- src/global-components/navbar/navbar-mobile.tsx
+++ src/global-components/navbar/navbar-mobile.tsx
@@ -1,5 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavbarModal, WrapperMobile } from './style.ts'
+import { HamburguerIcon } from '../../assets/HamburguerIcon.tsx'
+import { Button } from '../../styles'
+import AlfaLogo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
+import { CloseIcon } from '../../assets/CloseIcon.tsx'
 
 export function NavbarMobile() {
-  return <></>
+  const [isOpen, setIsOpen] = useState(false)
+
+  console.log(isOpen)
+
+  return (
+    <WrapperMobile display={isOpen ? 'open' : 'closed'}>
+      <Button onClick={() => setIsOpen((prev) => !prev)}>
+        <HamburguerIcon />
+      </Button>
+
+      <picture>
+        <img src={AlfaLogo} alt="AlfaLogo" />
+      </picture>
+
+      <NavbarModal transform={isOpen ? 'open' : 'closed'}>
+        <Link onClick={() => setIsOpen((prev) => !prev)} to={'/#home'}>
+          Início
+        </Link>
+
+        <Link
+          onClick={() => setIsOpen((prev) => !prev)}
+          to={'/#serviceDetails'}
+        >
+          Serviços
+        </Link>
+        <Link onClick={() => setIsOpen((prev) => !prev)} to={'/#course'}>
+          Tráfego Pago
+        </Link>
+        <Link onClick={() => setIsOpen((prev) => !prev)} to={'/#about'}>
+          Sobre
+        </Link>
+
+        <Button
+          variantType={'light'}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <CloseIcon />
+        </Button>
+      </NavbarModal>
+    </WrapperMobile>
+  )
 }
--- src/global-components/navbar/style.ts
+++ src/global-components/navbar/style.ts
@@ -5,6 +5,10 @@ export const WrapperDesktop = styled('div', {
   padding: '1rem',
   background: 'black',
 
+  '@mobile': {
+    display: 'none',
+  },
+
   '& > div': {
     maxWidth: '68.75rem',
     width: '100%',
@@ -25,6 +29,81 @@ export const WrapperDesktop = styled('div', {
 
     a: {
       color: 'white',
+    },
+  },
+})
+
+export const WrapperMobile = styled('div', {
+  backgroundColor: 'black',
+  display: 'flex',
+  justifyContent: 'space-between',
+  position: 'sticky',
+  left: '0',
+  top: '0',
+  zIndex: 100,
+
+  variants: {
+    display: {
+      open: {
+        '&:before': {
+          display: 'initial',
+        },
+      },
+      closed: {
+        '&:before': {
+          display: 'none',
+        },
+      },
+    },
+  },
+
+  '&:before': {
+    position: 'fixed',
+    zIndex: -1,
+    top: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    content: '',
+    background: 'black',
+    opacity: 0.6,
+  },
+
+  picture: {
+    display: 'block',
+    width: 'fit-content',
+    padding: '1rem',
+
+    img: {
+      width: '2.5rem',
+    },
+  },
+})
+
+export const NavbarModal = styled('nav', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '1rem',
+  gap: '1rem',
+  height: '100vh',
+  width: '80%',
+  left: '0',
+  top: '0',
+  transition: '300ms',
+  background: 'white',
+  zIndex: 100,
+  position: 'absolute',
+
+  variants: {
+    transform: {
+      open: {
+        transform: 'translateX(0%)',
+      },
+      closed: {
+        transform: 'translateX(-100%)',
+      },
     },
   },
 })
